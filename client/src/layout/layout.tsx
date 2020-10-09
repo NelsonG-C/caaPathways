292e517bc6578c6c8277a48251c253d1a1327827
Nelson Gardner-Challis <nelson@tfyc.com.au>
2020-10-09 13:11:55 +1100

    set up basic layout template

--- client/src/layout/layout.tsx
+++ client/src/layout/layout.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/styles.css";
 
-const Layout = (props: any) => {
-  return <div></div>;
+export const Layout = (props: any) => {
+  return (
+    <div>
+      <Navbar bg="light" expand={false}>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="mr-auto">
+          <Nav>
+            <Nav.Link href="#home">Majors</Nav.Link>
+            <Nav.Link>Problems</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand className="mx-auto">
+          <h4>Pathways</h4>
+        </Navbar.Brand>
+        <Nav.Link>About</Nav.Link>
+      </Navbar>
+      <Container>{props.children}</Container>
+      <footer>
+        <div>Cellular Agriculture Australia 2020</div>
+      </footer>
+    </div>
+  );
 };
