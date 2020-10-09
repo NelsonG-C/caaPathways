6a5fc32523194efe46ee6cbb46df5bc92a66b464
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-09 11:48:42 +1100

    basic site page layout added

---
+++ client/src/pages/about.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+// this may work as a modal instead
+
+const AboutPage = (props: any) => {
+  return <div></div>;
+};
---
+++ client/src/pages/major.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Major = (props: any) => {
+  return <div></div>;
+};
