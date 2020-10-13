e01a4611a645067aaabd8acc06b475075466bac9
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-13 20:12:09 +1100

    Homepage render added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Layout } from "./layout/layout";
+import { HomePage } from "./pages/home";
 
 function App() {
   return (
     <Layout>
+      <HomePage />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
