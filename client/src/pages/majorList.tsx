e0cd3aa98a86e98fa96ec757da751332939377af
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-13 20:57:59 +1100

    added in base dynamic card row rendering

--- client/src/pages/majorList.tsx
+++ client/src/pages/majorList.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import { CardItem } from "../components/card";
 
-const MajorList = (props: any) => {
-  return <div></div>;
+const mockData: object[] = [
+  { data: 1 },
+  { data: 2 },
+  { data: 3 },
+  { data: 1 },
+  { data: 2 },
+  { data: 3 },
+];
+
+export const MajorList = (props: any) => {
+  return (
+    <div>
+      <div className="filter-section"></div>
+      <Container>
+        <Row>
+          {mockData.map(() => {
+            return (
+              <Col xs={12} md={4} className="card-test">
+                <CardItem />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+      <h1>Hello major lis</h1>
+    </div>
+  );
 };
