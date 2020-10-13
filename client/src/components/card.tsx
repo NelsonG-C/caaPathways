afac0d91e17fc905380662a332acff00e18eb181
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-13 21:50:24 +1100

    added in base card layout

--- client/src/components/card.tsx
+++ client/src/components/card.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row, Button } from "react-bootstrap";
 import logo from "../logo.svg";
 
 export const CardItem = (props: any) => {
+  console.log("Props here", props);
   return (
-    <Card>
+    <Card className="cardItem">
       <Card.Img src={logo} />
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>This is test text</Card.Text>
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+          praesentium qui tempora quisquam modi autem facere error, aut quos
+          fugiat nesciunt at eius fugit dolores corporis atque, explicabo
+          voluptates debitis!
+        </Card.Text>
+        <Row>
+          <Button className="ml-3">Favourite</Button>
+          <Button className="ml-auto mr-3">Details</Button>
+        </Row>
+      </Card.Body>
     </Card>
   );
 };
