fdfd74b5848435a0dac385e65d64b4d6fd45620a
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-13 20:11:58 +1100

    basic homepage added

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Jumbotron, Button } from "react-bootstrap";
+import "../styles/styles.css";
 
-const HomePage = (props: any) => {
-  return <div></div>;
+export const HomePage = (props: any) => {
+  return (
+    <div>
+      <Jumbotron className="jumbotron-home">
+        <h1>Hello, world!</h1>
+        <h2>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </h2>
+        <div className="j-btn-row">
+          <Button variant="primary">Majors</Button>
+          <Button variant="primary">Problems to Solve</Button>
+        </div>
+      </Jumbotron>
+    </div>
+  );
 };
