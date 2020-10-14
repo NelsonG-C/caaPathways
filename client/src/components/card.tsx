17216f296cab1d519fc06f6f79771ca0ecb870be
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-14 12:54:13 +1100

    added dynamic blurb

--- client/src/components/card.tsx
+++ client/src/components/card.tsx
@@ -9,12 +9,7 @@ export const CardItem = (props: any) => {
       <Card.Img src={logo} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
-          praesentium qui tempora quisquam modi autem facere error, aut quos
-          fugiat nesciunt at eius fugit dolores corporis atque, explicabo
-          voluptates debitis!
-        </Card.Text>
+        <Card.Text>{props.blurb}</Card.Text>
         <Row>
           <Button className="ml-3">Favourite</Button>
           <Button className="ml-auto mr-3">Details</Button>
