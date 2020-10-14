1b7eca11466eed80942dfb189fe7bf6b2731ec9d
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-14 11:04:48 +1100

    single major hero image added

--- client/src/pages/major.tsx
+++ client/src/pages/major.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Jumbotron, Dropdown, Button } from "react-bootstrap";
+import logo from "../bg.jpg";
 
-const Major = (props: any) => {
-  return <div></div>;
+export const Major = (props: any) => {
+  return (
+    <div>
+      <div className="back-btn">
+        <Button>Back</Button>
+      </div>
+      <Jumbotron className="jumbotron-major">
+        <h1>Major Title</h1>
+      </Jumbotron>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias
+          reiciendis eos fugit itaque impedit? Libero maiores est eos,
+          doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta
+          sequi quis?
+        </p>
+      </div>
+    </div>
+  );
 };
