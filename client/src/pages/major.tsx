950b6622683cc28d3e8ade612ed2490a58818b74
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-14 11:47:26 +1100

    base dynamic expandable item list created

--- client/src/pages/major.tsx
+++ client/src/pages/major.tsx
@@ -1,6 +1,39 @@
 import React from "react";
 import { Jumbotron, Dropdown, Button } from "react-bootstrap";
-import logo from "../bg.jpg";
+import Collapsible from "react-collapsible";
+
+type testType = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+const mockProblems: testType[] = [
+  {
+    id: 1,
+    title: "Bioreactor Processing",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+  {
+    id: 2,
+    title: "Cell Lines",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+  {
+    id: 3,
+    title: "Cell Culture Media",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+  {
+    id: 4,
+    title: "Public Perception",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+];
 
 export const Major = (props: any) => {
   return (
@@ -18,6 +51,15 @@ export const Major = (props: any) => {
           doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta
           sequi quis?
         </p>
+      </div>
+      <div>
+        {mockProblems.map((data: testType) => {
+          return (
+            <Collapsible trigger={data.title}>
+              <p>{data.description}</p>
+            </Collapsible>
+          );
+        })}
       </div>
     </div>
   );
