ecd8e9dac9822a0a95dfa25eea23e0e19596f48e
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-14 12:54:30 +1100

    base problem list

--- client/src/pages/problemList.tsx
+++ client/src/pages/problemList.tsx
@@ -1,5 +1,51 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { CardItem } from "../components/card";
 
-const ProblemList = (props: any) => {
-  return <div></div>;
+const mockData = [
+  {
+    id: 1,
+    title: "Cell Lines",
+    blurb: "Small blurb here for the card before entering it",
+  },
+  {
+    id: 2,
+    title: "Media",
+    blurb: "Small blurb here for the card before entering it",
+  },
+  {
+    id: 3,
+    title: "Bioreactors",
+    blurb: "Small blurb here for the card before entering it",
+  },
+  {
+    id: 4,
+    title: "Marketing",
+    blurb: "Small blurb here for the card before entering it",
+  },
+  {
+    id: 5,
+    title: "Cell Lines",
+    blurb: "Small blurb here for the card before entering it",
+  },
+];
+
+export const ProblemList = (props: any) => {
+  return (
+    <div>
+      <div className="filter-section"></div>
+      <Container>
+        <Row>
+          {mockData.map((val: any) => {
+            return (
+              <Col xs={12} md={4} className="card-test" key={val.id}>
+                <CardItem title={val.title} blurb={val.blurb} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+      <h1>Hello major lis</h1>
+    </div>
+  );
 };
