8eff4b598b29cbd6718decb9889cb497f6097b93
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-15 14:26:41 +1100

    Added base function about modal

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Button, Modal, Carousel } from "react-bootstrap";
+import bg from "../bg.jpg";
 
-// this may work as a modal instead
+// trying as a modal
 
-const AboutPage = (props: any) => {
-  return <div></div>;
+export const AboutPage = (props: any) => {
+  const { show, handleClose } = props;
+  return (
+    <div>
+      <Modal show={show} onHide={handleClose}>
+        <Carousel interval={50000}>
+          <Carousel.Item>
+            <img className="d-block w-100" src={bg} alt="First slide" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={bg} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={bg} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+            <Button onClick={handleClose}>All Done Here</Button>
+          </Carousel.Item>
+        </Carousel>
+      </Modal>
+    </div>
+  );
 };
