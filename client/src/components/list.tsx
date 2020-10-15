0ec8f74b2a5664dde2a4b671a3bc6695e7e731d0
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-15 13:55:40 +1100

    base filter design and style added

--- client/src/components/list.tsx
+++ client/src/components/list.tsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import { CardItem } from "../components/card";
+
+const FilterNames = [
+  "Arts",
+  "Biology",
+  "Science",
+  "Software",
+  "Journalism",
+  "Design",
+  "Engineering",
+];
 
 export const List = (props: any) => {
   return (
     <div>
-      <div className="filter-section"></div>
+      <div className="filter-section">
+        <Container>
+          <Row className="filter-rows">
+            <Col>
+              <h2 className="filter-heading">Filter by Interest Area</h2>
+            </Col>
+            <Col>
+              {FilterNames.map((data, index) => {
+                return (
+                  <Button key={index} variant="outline-info">
+                    {data}
+                  </Button>
+                );
+              })}
+            </Col>
+          </Row>
+        </Container>
+      </div>
       <Container>
         <Row>
           {props.mockData.map((val: any) => {
@@ -17,7 +44,6 @@ export const List = (props: any) => {
           })}
         </Row>
       </Container>
-      <h1>Hello major lis</h1>
     </div>
   );
 };
