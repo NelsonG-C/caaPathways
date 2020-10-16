1aa7aa7e46f033479d860720b88507ed1ce2a023
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-16 19:20:42 +1100

    removed default leftover

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,29 +3,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Layout } from "./layout/layout";
 import { HomePage } from "./pages/home";
-import { MajorList } from "./pages/majorList";
+import { MajorListPage } from "./pages/majorList";
 import { Major } from "./pages/major";
+import { ProblemListPage } from "./pages/problemList";
 
 function App() {
   return (
     <Layout>
+      <HomePage />
+      <ProblemListPage />
+      <MajorListPage />
       <Major />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
     </Layout>
   );
 }
