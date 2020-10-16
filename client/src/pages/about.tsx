ac5d289443be1edfb77c6636b0267fc618e0fac7
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-16 19:21:15 +1100

    updated styling of modal

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -11,35 +11,30 @@ export const AboutPage = (props: any) => {
       <Modal show={show} onHide={handleClose}>
         <Carousel interval={50000}>
           <Carousel.Item>
-            <img className="d-block w-100" src={bg} alt="First slide" />
             <Carousel.Caption>
-              <h3>Welcome to Pathways</h3>
+              <h3>Welcome to</h3>
               <h1>PATHWAYS</h1>
               <p>Take your next step in Cellular Agriculture.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src={bg} alt="Third slide" />
             <Carousel.Caption>
               <h3>Search the way you want</h3>
               <p>Start with Majors or Problems to Solve.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src={bg} alt="Third slide" />
-            <Carousel.Caption>
+            <Carousel.Caption className="img-3">
               <h3>Filter results</h3>
               <p>Filter based on your specific interests</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src={bg} alt="Third slide" />
-
             <Carousel.Caption>
               <h3>Find your passion</h3>
               <p>Look at the results and see where you can help.</p>
+              <Button onClick={handleClose}>All Done Here</Button>
             </Carousel.Caption>
-            <Button onClick={handleClose}>All Done Here</Button>
           </Carousel.Item>
         </Carousel>
       </Modal>
