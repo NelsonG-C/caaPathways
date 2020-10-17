a3c230a93cc2230cd3ca930d6a9d105003ad4e9b
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-10-17 16:39:39 +1100

    added individual problem page

--- client/src/pages/problem.tsx
+++ client/src/pages/problem.tsx
@@ -1,5 +1,73 @@
 import React from "react";
+import { Jumbotron, Dropdown, Button } from "react-bootstrap";
+import Collapsible from "react-collapsible";
 
-const Problem = (props: any) => {
-  return <div></div>;
+type testType = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+const mockMajors: testType[] = [
+  {
+    id: 1,
+    title: "Agricultural Science",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+  {
+    id: 2,
+    title: "Neuroscience",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+  {
+    id: 3,
+    title: "Computer Science",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+  {
+    id: 4,
+    title: "Biomedicine",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias reiciendis eos fugit itaque impedit? Libero maiores est eos, doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta sequi quis?",
+  },
+];
+
+export const Problem = (props: any) => {
+  return (
+    <div>
+      <div className="back-btn">
+        <Button>Back</Button>
+      </div>
+      <div className="jumbo-shadow">
+        <Jumbotron className="jumbotron-major">
+          <h1>Problem Title</h1>
+        </Jumbotron>
+      </div>
+      <div className="major-txt">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias
+          reiciendis eos fugit itaque impedit? Libero maiores est eos,
+          doloremque eveniet enim, ipsum ipsam incidunt illo voluptate soluta
+          sequi quis?
+        </p>
+      </div>
+      <div>
+        <h2 className="major-txt">Relevant Majors</h2>
+        {mockMajors.map((data: testType) => {
+          return (
+            <Collapsible
+              classParentString="pm"
+              trigger={data.title}
+              key={data.id}
+            >
+              <p>{data.description}</p>
+            </Collapsible>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
