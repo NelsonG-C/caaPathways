a60a051780137b46fb7a2529415c44cb34b93f08
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-11-04 13:32:57 +1100

    fixed links

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Jumbotron, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "../styles/styles.css";
 
 export const HomePage = (props: any) => {
@@ -7,13 +8,17 @@ export const HomePage = (props: any) => {
     <div>
       <Jumbotron className="jumbotron-home">
         <h1>PATHWAYS</h1>
-        <h2>Take the next step for your career in Cellular Agriculture.</h2>
+        <h2>Take the next step for your career in Cellular Agriculture</h2>
         <div className="j-btn-row">
           <Button className="green-btn">
-            Majors <span>&rarr;</span>
+            <Link className="nav-link" to="/majors">
+              Majors <span>&rarr;</span>
+            </Link>
           </Button>
           <Button className="green-btn">
-            Problems to Solve <span>&rarr;</span>
+            <Link className="nav-link" to="/problems">
+              Problems to Solve <span>&rarr;</span>
+            </Link>
           </Button>
         </div>
       </Jumbotron>
